6898f34b8052740c739bf6d553306568e053a044
Shefaa <shefaasomar@gmail.com>
2022-09-21 21:31:28 +0300

    fix: form empty data

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,20 +1,36 @@
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 import { handleSaveQuestion } from "../actions/shared";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const NewQuestion = (props) => {
+  const [optionOne, setOptionOne] = useState("");
+  const [optionTwo, setOptionTwo] = useState("");
+
   const navigate = useNavigate();
+
+  const handleInput = (event, option) => {
+    const value = event.target.value;
+    if (option === "optionOne") setOptionOne(value);
+    else setOptionTwo(value);
+  };
+
   const handleSubmitQuestion = (event) => {
     event.preventDefault();
+
     const option1 = event.target[0].value;
     const option2 = event.target[1].value;
 
     props.dispatch(handleSaveQuestion(option1, option2));
     navigate("/");
   };
+
+  const formCompleted = () => {
+    return optionOne !== "" && optionTwo !== "";
+  };
+
   return (
     <div
       style={{ margin: "80px", paddingLeft: "100px", paddingRight: "100px" }}
@@ -25,20 +41,39 @@ const NewQuestion = (props) => {
       <Form onSubmit={handleSubmitQuestion}>
         <Form.Group className="mb-3" controlId="option1">
           <Form.Label>Option 1</Form.Label>
-          <Form.Control type="text" placeholder="Enter option 1" />
+          <Form.Control
+            type="text"
+            placeholder="Enter option 1"
+            onChange={(event) => handleInput(event, "optionOne")}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="option2">
           <Form.Label>Option 2</Form.Label>
-          <Form.Control type="text" placeholder="Enter option 2" />
+          <Form.Control
+            type="text"
+            placeholder="Enter option 2"
+            onChange={(event) => handleInput(event, "optionTwo")}
+          />
         </Form.Group>
-        <Button
-          variant="outline-primary"
-          type="submit"
-          style={{ marginTop: "20px" }}
-        >
-          Submit
-        </Button>
+        {formCompleted() ? (
+          <Button
+            variant="outline-primary"
+            type="submit"
+            style={{ marginTop: "20px" }}
+          >
+            Submit
+          </Button>
+        ) : (
+          <Button
+            variant="outline-primary"
+            type="submit"
+            style={{ marginTop: "20px" }}
+            disabled
+          >
+            Submit
+          </Button>
+        )}
       </Form>
     </div>
   );
